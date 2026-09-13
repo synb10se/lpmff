1097041836aabe2d9deded5d85367f81b548db3f
Sascha Vogel <sascha.vogel@synvoll.de>
2026-09-13 17:38:04 +0200

    just another syntax error

--- kbastatistiken/index.php
+++ kbastatistiken/index.php
@@ -59,6 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($temporaryOutput !== false && is_file($temporaryOutput)) {
             unlink($temporaryOutput);
         }
+      }
     }
 }
 ?>
